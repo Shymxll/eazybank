d9a975b3e0604a21eb75a8e3ee39081aed51c568
Shamkhal Huseynzade <shamkhalhuseynzade26@gmail.com>
2023-08-07 10:34:38 +0300

    basic auth config

---
+++ src/main/java/com/project/eazybank/controller/ContactController.java
@@ -0,0 +1,13 @@
+package com.project.eazybank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+    
+    @GetMapping("/contact")
+    public String getContactDetails() {
+        return "Contact";
+    }
+}
---
+++ src/main/java/com/project/eazybank/controller/LoansController.java
@@ -0,0 +1,13 @@
+package com.project.eazybank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoansController {
+    
+    @GetMapping("/myLoans")
+    public String getLoansDetails() {
+        return "Loans";
+    }
+}
